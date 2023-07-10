6bffd6a99c62ce2f56d4bcc57f7f0469a07eb0b7
kandekore <darren@kandekore.net>
2023-07-10 22:58:45 +0100

    Update woocollect.php

--- woocollect.php
+++ woocollect.php
@@ -292,9 +292,14 @@ function collection_time_booking_add_meta_box($checkout)
     $selected_date = '';
     $selected_time = '';
 
-    $time_slots[''] = "Select Collection Time";
-    
+    $time_slots[''] = "Select Collection Time";//anuj
     // Generate time slots based on the opening hours
+    
+    //anuj
+    // for ($time = $start_time; $time < $end_time; $time += $minimum_interval) {
+    //     $time_slots[date('H:i', $time)] = date('h:i A', $time);
+    // }
+    //anuj
 
     echo '<div id="collection-time-box">';
     woocommerce_form_field(
@@ -428,58 +433,21 @@ function collection_time_booking_display_admin_order_meta($order)
     }
 }
 
-// Attach collection date and time to the order confirmation email sent to the admin and customers
-add_filter('woocommerce_email_order_meta_fields', 'collection_time_booking_add_collection_datetime_to_email', 10, 3);
+// Attach collection date and time to the order confirmation email sent to the admin
+add_action('woocommerce_email_order_details', 'collection_time_booking_add_collection_datetime_to_email', 10, 4);
 
-function collection_time_booking_add_collection_datetime_to_email($fields, $sent_to_admin, $order)
+function collection_time_booking_add_collection_datetime_to_email($order, $sent_to_admin, $plain_text, $email)
 {
-       // Check if the order has the collection date and time meta
-    $collection_date = $order->get_meta('Collection Date');
-    $collection_time = $order->get_meta('Collection Time');
-    $pickup_location = $order->get_meta('Pickup Location');
-    $branch_address = $order->get_meta('Branch Address');
-
-    if (!empty($collection_date) && !empty($collection_time)) {
-        $fields['collection_date'] = array(
-            'label' => __('Collection Date', 'collection-time-booking'),
-            'value' => $collection_date,
-        );
-        $fields['collection_time'] = array(
-            'label' => __('Collection Time', 'collection-time-booking'),
-            'value' => $collection_time,
-        );
-
-        if (!empty($pickup_location) && !empty($branch_address)) {
-            $fields['pickup_location'] = array(
-                'label' => __('Pickup Location', 'collection-time-booking'),
-                'value' => $pickup_location,
-            );
-            $fields['branch_address'] = array(
-                'label' => __('Branch Address', 'collection-time-booking'),
-                'value' => $branch_address,
-            );
-        }
-    }
-    return $fields;
-}
-
-// Display the selected collection date and time on the order-received page
-add_action('woocommerce_thankyou', 'collection_time_booking_display_order_received_collection_datetime', 10, 1);
-
-function collection_time_booking_display_order_received_collection_datetime($order_id)
-{
-    $order = wc_get_order($order_id);
-    $collection_date = $order->get_meta('Collection Date');
-    $collection_time = $order->get_meta('Collection Time');
-
-    if (!empty($collection_date) && !empty($collection_time)) {
-        echo '<div class="order-received-collection-datetime">';
-        echo '<h2>' . __('Collection Date and Time', 'collection-time-booking') . '</h2>';
-        echo '<p><strong>' . __('Collection Date', 'collection-time-booking') . ':</strong> ' . esc_html($collection_date) . '</p>';
-        echo '<p><strong>' . __('Collection Time', 'collection-time-booking') . ':</strong> ' . esc_html($collection_time) . '</p>';
-        echo '</div>';
+    if ($sent_to_admin && $order->get_meta('Collection Date') && $order->get_meta('Collection Time')) {
+        $collection_date = $order->get_meta('Collection Date');
+        $collection_time = $order->get_meta('Collection Time');
+        $collection_datetime = date('Y-m-d H:i', $order->get_meta('Collection DateTime'));
+        echo '<p><strong>Collection Date:</strong> ' . esc_html($collection_date) . '</p>';
+        echo '<p><strong>Collection Time:</strong> ' . esc_html($collection_time) . '</p>';
+        echo '<p><strong>Collection DateTime:</strong> ' . esc_html($collection_datetime) . '</p>';
     }
 }
+
 // Add custom dashboard widget
 add_action('wp_dashboard_setup', 'collection_time_booking_add_dashboard_widget');
 
@@ -618,3 +586,4 @@ function remove_field_validation_on_shipping_change( $posted_data ) {
     return $posted_data;
 }
 add_filter( 'woocommerce_checkout_posted_data', 'remove_field_validation_on_shipping_change',10,2);
+
