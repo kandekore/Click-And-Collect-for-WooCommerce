3d516048fd0577b46da6af0adbb32bbcbe73a073
Darren <you@example.comdarren@kandekore.net>
2023-11-21 17:39:43 +0000

    lcoalize scripts & deduplicate enque

--- woocollect.php
+++ woocollect.php
@@ -534,49 +534,39 @@ function display_future_collection_orders() {
 
 function enqueue_my_script() {
     $selected_shipping_methods = get_option('click_collect_shipping_methods', array());
-    wp_enqueue_script('collection-time-booking-script', plugin_dir_url(__FILE__) . 'js/collection-time-booking.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('my-shipping-methods-script', plugin_dir_url(__FILE__) . 'js/shipping-methods.js', array('jquery', 'collection-time-booking-script'), '1.1.1', true);
+    $booking_window_hours = get_option('booking_window_hours', 2); // Default to 2 if not set
+
+    // Enqueue scripts and styles
+    wp_enqueue_script('jquery-ui-core');
+    wp_enqueue_script('jquery-ui-datepicker');
+    wp_enqueue_style('jquery-ui-datepicker-css', plugins_url('assets/jquery-ui.css', __FILE__));
+    wp_enqueue_script('jquery-ui-timepicker-addon', plugins_url('assets/jquery-ui-timepicker-addon.min.js', __FILE__), array('jquery-ui-datepicker'), '1.6.3', true);
+    wp_enqueue_style('jquery-ui-timepicker-css', plugins_url('assets/jquery-ui-timepicker-addon.min.css', __FILE__));
+    wp_enqueue_script('collection-time-booking-script', plugin_dir_url(__FILE__) . 'js/collection-time-booking.js', array('jquery-ui-datepicker', 'jquery-ui-timepicker-addon'), '1.19', true);
+    wp_enqueue_style('plugin-styles', plugin_dir_url(__FILE__) . 'plugin-styles.css');
+
+    // Prepare and localize script
     wp_localize_script('collection-time-booking-script', 'my_script_vars', array(
-        'selected_shipping_methods' => implode(" , ",$selected_shipping_methods),
+        'selected_shipping_methods' => implode(" , ", $selected_shipping_methods),
     ));
-    
-    // Localize script with the collection time options
-$booking_window_hours = get_option('booking_window_hours', 2); // Get booking window hours from settings, default to 2 if not set
 
-$collection_time_options = array(
-    'curdate' => date("Y-m-d"),
-    'timeFormat' => get_option('time_format', 'g:i A'),
-    'minDate' => 0, // Minimum date is today
-    'minTime' => date('H:i', strtotime('+' . $booking_window_hours . ' hours')), // Minimum time is 'booking_window_hours' hours from now
-    'maxTime' => '' // Placeholder for the maximum time based on opening hours
-);
-$opening_hours = get_option('collection_time_booking_opening_hours', array());
-if (!empty($opening_hours)) {
-    $collection_time_options['openingHours'] = $opening_hours;
-}
-    
-wp_localize_script('collection-time-booking-script', 'collectionTimeOptions', $collection_time_options);
+    $collection_time_options = array(
+        'curdate' => date("Y-m-d"),
+        'timeFormat' => get_option('time_format', 'g:i A'),
+        'minDate' => 0, // Minimum date is today
+        'minTime' => date('H:i', strtotime('+' . $booking_window_hours . ' hours')), // Minimum time is 'booking_window_hours' hours from now
+        'maxTime' => '' // Placeholder for the maximum time based on opening hours
+    );
 
-// Enqueue jQuery UI
-wp_enqueue_script('jquery-ui-core');
-wp_enqueue_script('jquery-ui-datepicker');
+    $opening_hours = get_option('collection_time_booking_opening_hours', array());
+    if (!empty($opening_hours)) {
+        $collection_time_options['openingHours'] = $opening_hours;
+    }
 
-// Enqueue jQuery UI CSS
-wp_enqueue_style('jquery-ui-datepicker-css', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
-
-// Enqueue time picker JavaScript
-wp_enqueue_script('jquery-ui-timepicker-addon', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.3/jquery-ui-timepicker-addon.min.js', array('jquery-ui-datepicker'), '1.6.3', true);
-
-// Enqueue time picker CSS
-wp_enqueue_style('jquery-ui-timepicker-css', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.3/jquery-ui-timepicker-addon.min.css');
-
-// Enqueue custom JavaScript for initializing date and time pickers
-wp_enqueue_script('collection-time-booking-script', plugin_dir_url(__FILE__) . 'js/collection-time-booking.js', array('jquery-ui-datepicker', 'jquery-ui-timepicker-addon'),'1.19', true);//
-
-// Enqueue css
-wp_enqueue_style( 'plugin-styles', plugin_dir_url( __FILE__ ) . 'plugin-styles.css' );
+    wp_localize_script('collection-time-booking-script', 'collectionTimeOptions', $collection_time_options);
 }
 add_action('wp_enqueue_scripts', 'enqueue_my_script');
+
 
 
 add_action('admin_init', 'register_booking_window_settings');
